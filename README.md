2a0de8a768b3a77411ecace260ae9510c3bdaaf3
fir <664339636@qq.com>
2016-10-13 11:06:32 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # f-lazyload
 一个懒加载库，无依赖
+兼容IE8(不包括IE8)以上
+
+```
+实现的主要功能:
+1、支持img,canvas,div(能设置background-image属性)标签元素
+2、支持img设置dataset属性
+3、自定义加载完成动画
+4、自定义loading图片和error图片
+5、支持预加载
+```
+
+### 安装依赖
+
+> npm install
+
+### 开发
+
+> gulp dev 浏览器自动打开localhost:3000
+
+### 打包
+
+> gulp build 查看dist目录
+
+
+### 如何使用
+
+1、页面引入
+```
+<script src="youpath/f-lazload.js"></script>
+
+var Flazyload = new Flazyload();
+```
+###or:###
+
+2、npm安装
+
+> npm install --save f-lazyload
+
+```
+var Flazyload = require('f-lazyload');
+new Flazyload();
+```
+
+
+
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+var gulp = require('gulp'),
+	changed = require('gulp-changed'),
+	uglify = require('gulp-uglify'),
+	plumber = require('gulp-plumber'),
+	clean = require('gulp-clean'),
+	rename = require('gulp-rename'),
+	runSequence = require('run-sequence'),
+	browserSync = require('browser-sync').create(),
+	reload = browserSync.reload;
+
+var jsPath = './src/*.js';
+	htmlPath = './test/*.html';
+
+//gulp dev
+gulp.task('dev', function(callback) {
+	runSequence(
+		'clean',
+		'browser-sync',
+		callback
+	)
+});
+//gulp build
+gulp.task('build', function(callback) {
+	runSequence(
+		'clean',
+		'min:js',
+		callback
+	)
+});
+//clean
+gulp.task('clean', function(){
+	return gulp.src('dist', {
+			read: false
+		})
+		.pipe(clean());
+});
+gulp.task('js', function(){
+	return gulp.src(jsPath)
+		.pipe(changed(jsPath))
+		.pipe(plumber())
+		.pipe(gulp.dest('./dist'))
+});
+gulp.task('html', function(){
+	return gulp.src(htmlPath)
+		.pipe(changed(htmlPath))
+		.pipe(plumber())
+		.pipe(gulp.dest('./dist'))
+});
+gulp.task('min:js', function(){
+	return gulp.src(jsPath)
+		.pipe(gulp.dest('./dist'))
+		.pipe(uglify())
+		.pipe(rename({
+			    dirname: ".",
+			    basename: "f-lazyload",
+			    suffix: ".min",
+			    extname: ".js"
+		}))
+		.pipe(gulp.dest('./dist'))
+});
+//browser-sync
+gulp.task('browser-sync', ['js', 'html'], function() {
+	browserSync.init({
+		//browser: "google chrome",
+		server: {
+			baseDir: "./dist"//多个基目录
+		},
+		port: 3000 //default 3000
+			/*logFileChanges: true,
+			logConnections: true*/
+	});
+	gulp.watch(jsPath, ['js']);
+	gulp.watch(htmlPath, ['html']);
+});
