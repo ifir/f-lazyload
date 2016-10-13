5b42d5fa45f26d4d6a1dccbdb04e94cc76f641e1
fir <664339636@qq.com>
2016-10-13 17:15:58 +0800

    update

--- src/f-lazyload.js
+++ src/f-lazyload.js
@@ -14,9 +14,12 @@
 	}
 })(typeof window !== 'undefined' ? window : this, function(){
 	'use strict';
+
 	function Flazyload(opts){
 		opts = arguments.length > 0 ? opts : {};
 		var _this = this;
+		//指定父容器
+		_this.container = opts.container || 'body';
 		//默认添加data-src属性开启懒加载
 		_this.src = opts.sign || 'data-src';
 		//srcset属性
@@ -24,7 +27,8 @@
 		//默认img标签
 		_this.tag = opts.tag ||'img';
 		//获取需要懒加载的dom
-		_this.eles = document.querySelectorAll(_this.tag+'['+_this.src+']');
+		//_this.eles = document.querySelectorAll(_this.container+' '+ _this.tag+'['+_this.src+']');
+		_this.eles = document.querySelectorAll(_this.container+' *['+_this.src+']');
 		_this.length = _this.eles.length;
 		//是否在页面首次载入就判断是否有需要懒加载的图片在可视区域
 		_this.preload = opts.proload || true;
@@ -52,10 +56,11 @@
 			var _this = this;
 			//设置loading图片
 			if(_this.loadimg){
-				for(var i = 0; i < this.length; i++){
+				for(var i = 0; i < _this.length; i++){
 					_this.eles[i].setAttribute('src', _this.loadimg);
 				}
 			}
+			//首次载入页面是否判断懒加载元素是否在可视区域内
 			_this.preload && _this.onread(_this);
 		},
 		//是否在可视区域
@@ -67,7 +72,7 @@
 	        var eHeight = rect.height;
 	        return eTop < this.winH && eTop + eHeight >= 0 && eLeft < this.winW && eLeft + eWidth >= 0;
 		},
-		//首次打开页面执行懒加载
+		//需要执行的懒加载
 		onread: function(_this){
 			var isload = false, remove = false;
 			for(var i = 0; i < _this.length; i++){
@@ -77,7 +82,7 @@
 				}
 			}
 			if(isload){
-				_this.eles = document.querySelectorAll(_this.tag+'['+_this.src+']');
+				_this.eles = document.querySelectorAll(_this.container+' *['+_this.src+']');
 				_this.length = _this.eles.length;
 				isload = false;
 			}
@@ -101,17 +106,24 @@
 			//图片加载失败
 			img.addEventListener('error', function(){
 				//设置图片加载失败图片
-				var timer = null;
-				clearTimeout(timer);
-				timer = setTimeout(function(){
-					_this.errimg && ele.setAttribute('src', _this.errimg);
-					ele.removeAttribute(dataSrc);
+				if(_this.errimg){
+					var timer = null;
 					clearTimeout(timer);
-				},2000);
+					timer = setTimeout(function(){
+						ele.setAttribute('src', _this.errimg);
+						ele.removeAttribute(dataSrc);
+						clearTimeout(timer);
+					},2000);
+				}else{
+					ele.removeAttribute(dataSrc);
+					//ele.setAttribute('alt', '图片加载失败');
+				}
+				var timer = null;
 			}, false);
 			//发起请求
 			src !== '' && (img.src = src);
 		},
+		//绑定事件
 		addEvents: function(_this){
 			window.addEventListener('scroll', function(){
 				_this.onread(_this);
